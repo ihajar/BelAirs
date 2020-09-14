6bcd076900f0c9e340797b373967306541f7a14b
ihajar <hadjero.hamaidi@gmail.com>
2020-09-14 14:19:59 +0100

    first commit homepage and Ui design

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Welcome to hostair app, here you can find your house dream</h1>
+    </div>
+  );
+}
+
+export default App;
