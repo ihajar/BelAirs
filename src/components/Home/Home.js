5330c1607d0d9bd4d8e077fcaa2d6dc7b94dfc1b
ihajar <hadjero.hamaidi@gmail.com>
2020-09-20 20:43:50 +0100

    landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import * as FaIcons from 'react-icons/fa';
 import  * as AiIcons from 'react-icons/ai';
+import * as ImIcons from 'react-icons/im';
 
 import { Link } from 'react-router-dom';
 import { SidebarData } from '../Sidebar/SidebarData';
@@ -42,22 +43,28 @@ function Home() {
             >
                 <source src={BelAirsVid} type="video/mp4" />
             </video>
+
             <IconContext.Provider value={{ color: '#fff' }} >
                 <header className="header">
                     <div className="brand">
                         <Link to="#" className="menu-bars" >
                             <FaIcons.FaBars onClick={showSidebar} className="header__menu" />
+                            <h4>Menu</h4>
                         </Link>
-                        {/* <h4>Menu</h4> */}
-                        <h4>Apartments</h4>
+                        
+                        <ul className="header__list">
+                            <li>Apartments</li>
+                        </ul>
                     </div>
                     <div className="logo">
                         <h4>BelAirs</h4>
                     </div>
                     <div className="header__links">
-                        <h4>About</h4>
-                        <h4>Contact</h4>
-                        <h4>Help</h4>
+                        <ul className="header__list">
+                            <li>About</li>
+                            <li>Contact</li>
+                            <li>Help</li>
+                        </ul>
                     </div>
                 </header>
 
@@ -88,40 +95,53 @@ function Home() {
                                 <p>100A George St, Croydon CRO 1Gp </p>
                                 <h5>Call Us</h5>
                                 <p>
-                                    <AiIcons.AiOutlineMobile className="phone" /> +44(0)20 3548 6589 
+                                    {/* <AiIcons.AiOutlineMobile className="phone" /> +44(0)20 3548 6589  */}
+                                    <ImIcons.ImMobile className="phone" /> +44(0)20 3548 6589 
                                 </p>
                             </div>
                             <div className="nav-socials">
                                 <div className="nav-socials-icons">
-                                   <FaIcons.FaInstagram/>
+                                   <FaIcons.FaInstagram className="socials" />
                                 </div>
                                 <div className="nav-socials-icons">
-                                    <FaIcons.FaFacebook/>
+                                    <FaIcons.FaFacebookF className="socials" />
                                 </div>
-                                
+                                <div className="nav-socials-icons">
+                                    <FaIcons.FaTwitter className="socials" />
+                                </div>
+                                <div className="nav-socials-icons">
+                                    <FaIcons.FaYoutube className="socials" />
+                                </div>
                             </div>
                         </div>
                 </nav>
            </IconContext.Provider>
-
-           {/* <aside className="home__sidebar">
-               <IconButton onClick={closeMenu}>
-                   <CloseIcon className="sidebar__close" />
-               </IconButton>
-               <div>
-                    <ul>
-                        <li>Home</li>
-                        <li>Apartments</li>
-                        <li>Why Bel Airs</li>
-                        <li>Our Location</li>
-                        <li>Contact</li>
-
-                    </ul>
+    
+           <section className="home__body">
+               <div className="home__socials">
+                   <div className="home__SM">
+                       <FaIcons.FaFacebookF className="home__SMicon" />
+                   </div>
+                   <div className="home__SM">
+                       <FaIcons.FaInstagram className="home__SMicon" />
+                   </div>
+                   <div className="home__SM">
+                       <FaIcons.FaTwitter className="home__SMicon" />
+                   </div>
+                   <div className="home__SM">
+                       <FaIcons.FaYoutube className="home__SMicon" />
+                   </div>
                </div>
-               
+               <div className="home__content">
+                   <h2>Premium Rental Apartments </h2>
+                   <h1>Feel at home wherever you are</h1>
+                   <div className="home__button">
+                        <button>Search apartments </button>
+                   </div>
+                  
+               </div>
 
-           </aside> */}
-                
+           </section>
         </div>
     )
 }
